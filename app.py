423d77908d2c6fae54df2b7a3a450c630d10e662
dgsreissilva-create <dgsreissilva@gmail.com>
2026-02-24 20:56:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ else:
 
         if menu == "1. Cadastro de Médicos":
             st.header("👨‍⚕️ Cadastro de Médicos")
-            especialidades = ["Cardiologia", "Clinica", "Dermatologia", "Endocrinologia", "Fonoaudiologia", "Ginecologia", "Neurologia", "Neuropsicologia", "ODONTOLOGIA", "Oftalmologia", "Ortopedia", "Pediatria", "Psicologia"]
+            especialidades = ["Cardiologia", "Clinica", "Dermatologia", "Endocrinologia", "Endocrinologia - Diabete e Tireoide", "Fonoaudiologia", "Ginecologia", "Nefrologia", "Neurologia", "Neuropsicologia", "ODONTOLOGIA", "Oftalmologia", "Ortopedia", "Otorrinolaringologia", "Pediatria", "Pneumologia", "Psicologia"]
             with st.form("f_med"):
                 n = st.text_input("Nome do Médico")
                 e = st.selectbox("Especialidade", especialidades)
