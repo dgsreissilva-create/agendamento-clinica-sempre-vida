05e67782342178609616e479386418c110860f09
dgsreissilva-create <dgsreissilva@gmail.com>
2026-03-23 21:33:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ menu = st.sidebar.radio("Navegação", [
     "7. Excluir Cadastro de Médico",
     "8. Relatório Gerencial",
     "9. Gestão de Especialidades",
-    "10. Recepção e Triagem"
+    "10. Recepcionar Pacientes"
     
 ], index=2)
 
